108c86a6db0a117c789e217d486a35c907cde9d1
Kimtai Lewis <lewis.kimtai@gmail.com>
2025-01-21 22:03:03 +0300

    Refactor SaccoHeader component; simplify layout, remove unused imports, and adjust styles for better spacing

--- componets/saccos/SaccoHeader.tsx
+++ componets/saccos/SaccoHeader.tsx
@@ -1,6 +1,5 @@
 import {
   Modal,
-  Platform,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -10,9 +9,6 @@ import {
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useState } from "react";
 import { Link } from "expo-router";
-import Colors from "@/constants/Colors";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SaccoHeader() {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -21,103 +17,105 @@ export default function SaccoHeader() {
     setIsModalVisible(!isModalVisible);
   };
   return (
-    <>
-      <StatusBar style="dark" />
-      <View style={styles.headerWrapper}>
-        <View style={{ marginTop: 10 }}>
-          <Text style={{ fontSize: 28, fontWeight: "900" }}>SACCO NAME</Text>
-        </View>
+    <View style={styles.headerWrapper}>
+      <View>
+        <Text style={{ fontSize: 28, fontWeight: "900" }}>SACCO NAME</Text>
+      </View>
 
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginTop: 5,
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={{ flexDirection: "column" }}>
+          <Text style={{ fontSize: 17, fontWeight: "500" }}>SAVINGS</Text>
+          <Text style={{ fontSize: 28, fontWeight: "900" }}>2,000,000</Text>
+        </View>
         <View
           style={{
             flexDirection: "row",
-            alignItems: "center",
-            marginTop: 5,
-            justifyContent: "space-between"
+            justifyContent: "space-between",
+            gap: 40,
           }}
         >
-          <View style={{ flexDirection: "column" }}>
-            <Text style={{ fontSize: 17, fontWeight: "500" }}>SAVINGS</Text>
-            <Text style={{ fontSize: 28, fontWeight: "900" }}>2,000,000</Text>
-          </View>
-          <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
-            <View style={{ flexDirection: "column" }}>
-              <Text style={{ fontSize: 17, fontWeight: "500" }}>
-                INTEREST EARNED
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <Text style={{ fontSize: 17, fontWeight: "500" }}>
+              INTEREST EARNED
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 25,
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ fontSize: 20, fontWeight: "900", color: "green" }}>
+                200,000
               </Text>
-              <View
-                style={{ flexDirection: "row", gap: 30, alignItems: "center" }}
-              >
-                <Text
-                  style={{ fontSize: 20, fontWeight: "900", color: "green" }}
-                >
-                  200,000
-                </Text>
-                <Text
-                  style={{ fontSize: 17, fontWeight: "900", color: "green" }}
-                >
-                  80%
-                </Text>
-              </View>
+              <Text style={{ fontSize: 17, fontWeight: "900", color: "green" }}>
+                80%
+              </Text>
             </View>
-            <View style={styles.container}>
-              <TouchableOpacity onPress={toggleModal}>
-                <FontAwesome name="bars" size={24} color="black" />
-              </TouchableOpacity>
+          </View>
+          <View style={styles.container}>
+            <TouchableOpacity onPress={toggleModal}>
+              <FontAwesome name="bars" size={24} color="black" />
+            </TouchableOpacity>
 
-              <Modal
-                animationType="slide"
-                transparent={true}
-                visible={isModalVisible}
-                onRequestClose={toggleModal}
-              >
-                <TouchableWithoutFeedback onPress={toggleModal}>
-                  <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                      <Link href={"/LoanRequest"} asChild>
-                        <TouchableOpacity style={styles.modalTextButton}>
-                          <Text style={styles.modalText}>Request a Loan</Text>
-                        </TouchableOpacity>
-                      </Link>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={isModalVisible}
+              onRequestClose={toggleModal}
+            >
+              <TouchableWithoutFeedback onPress={toggleModal}>
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                    <Link href={"/LoanRequest"} asChild>
+                      <TouchableOpacity style={styles.modalTextButton}>
+                        <Text style={styles.modalText}>Request a Loan</Text>
+                      </TouchableOpacity>
+                    </Link>
 
-                      <Link href={"/AddNewMember"} asChild>
-                        <TouchableOpacity style={styles.modalTextButton}>
-                          <Text style={styles.modalText}>Add new member</Text>
-                        </TouchableOpacity>
-                      </Link>
+                    <Link href={"/AddNewMember"} asChild>
+                      <TouchableOpacity style={styles.modalTextButton}>
+                        <Text style={styles.modalText}>Add new member</Text>
+                      </TouchableOpacity>
+                    </Link>
 
-                      <Link href={"/Approvals"} asChild>
-                        <TouchableOpacity style={styles.modalTextButton}>
-                          <Text style={styles.modalText}>Approvals </Text>
-                        </TouchableOpacity>
-                      </Link>
+                    <Link href={"/Approvals"} asChild>
+                      <TouchableOpacity style={styles.modalTextButton}>
+                        <Text style={styles.modalText}>Approvals </Text>
+                      </TouchableOpacity>
+                    </Link>
 
-                      <Link href={"/documents"} asChild>
-                        <TouchableOpacity style={styles.modalTextButton}>
-                          <Text style={styles.modalText}>Sacco Documents</Text>
-                        </TouchableOpacity>
-                      </Link>
-                    </View>
+                    <Link href={"/documents"} asChild>
+                      <TouchableOpacity style={styles.modalTextButton}>
+                        <Text style={styles.modalText}>Sacco Documents</Text>
+                      </TouchableOpacity>
+                    </Link>
                   </View>
-                </TouchableWithoutFeedback>
-              </Modal>
-            </View>
+                </View>
+              </TouchableWithoutFeedback>
+            </Modal>
           </View>
         </View>
       </View>
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   headerWrapper: {
-    height: 90,
+    height: 80,
     justifyContent: "center",
     paddingHorizontal: 15,
+    marginVertical: 10
   },
   container: {
-    paddingTop: 10,
-    marginLeft: 30
   },
   centeredView: {
     flex: 1,
--- componets/saccos/ToptabBar.tsx
+++ componets/saccos/ToptabBar.tsx
@@ -23,7 +23,7 @@ export function TopTabBar({
     <>
       <StatusBar style="light" />
       <SafeAreaView style={styles.safeAreaView}>
-        <View style={{ gap: 10 }}>
+        <View >
           <SaccoHeader />
           <View style={styles.container}>
             {state.routes
